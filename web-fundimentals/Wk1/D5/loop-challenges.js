8831c8990f7d324e0eb3a9a02a54402185187a21
Mendy Giter <mendy@leskanharvax.com>
2022-04-24 17:24:40 -0400

    commit

---
+++ web-fundimentals/Wk1/D5/loop-challenges.js
@@ -0,0 +1,14 @@
+//Print odds
+var myArray = [];
+for (var i = 1; i < 20; i += 2){
+    myArray.push(i);
+}
+console.log(myArray);
+
+//Decreasing Multiples of 3
+var myArray = [];
+for(var i = 100; i > 0; i-=3) {
+    myArray.push(i);
+}
+console.log(myArray);
+

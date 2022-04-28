6116d23682ae640583254e06803655de454037cb
Mendy Giter <mendy@leskanharvax.com>
2022-04-28 14:51:25 -0400

    commit

---
+++ web-fundimentals/Wk1/Wk2/D2/dojo-pizza.js
@@ -0,0 +1,49 @@
+
+
+function pizzaOven (crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza1 = pizzaOven("deep dish", "traditional", "mozzorela", ["pepperoni", "sausage"]);
+console.log(pizza1);
+
+function pizzaOven (crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozorela", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+
+function pizzaOven (crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza3 = pizzaOven("deep dish", "traditional", "mozzorela", ["peperoni", "pineapple"]);
+console.log(pizza3);
+
+function pizzaOven (crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza4 = pizzaOven("deep dish", "traditional", "mozzorela", ["onions", "jalapenos"]);
+console.log(pizza4);

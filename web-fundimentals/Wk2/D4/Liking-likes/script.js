eee8806ddb25a4ade4e49d3c6ffe3a98688da231
Mendy Giter <mendy@leskanharvax.com>
2022-04-28 19:06:14 -0400

    commit

--- web-fundimentals/Wk2/D4/Liking-likes/script.js
+++ web-fundimentals/Wk2/D4/Liking-likes/script.js
@@ -1,10 +1,21 @@
-var count = 3;
-var countElement = document.querySelector("#count")
+// var count = 3;
+// var countElement = document.querySelectorAll("#count")
 
-console.log(countElement);
+// console.log(countElement);
 
-function add1() {
-    count++;
-    countElement.innerText = count + " Like(s)";
-    console.log(count);
+// function add1() {
+//     count++;
+//     countElement.innerText = count + " Like(s)";
+//     console.log(count);
+// }
+
+var likes = [3,3,3]
+var query = [
+    document.querySelector("#count1"),
+    document.querySelector("#count2"),
+    document.querySelector("#count3"),
+]
+function add1 (i) {
+    likes[i]++
+    query[i].innerText = likes[i] + " likes(s)";
 }

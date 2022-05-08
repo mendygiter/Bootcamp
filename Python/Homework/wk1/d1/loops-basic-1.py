8ee6e4655115535f150b98b85e73080930936be8
Mendy Giter <mendy@leskanharvax.com>
2022-05-08 15:42:56 -0400

    commit

--- Python/Homework/wk1/d1/loops-basic-1.py
+++ Python/Homework/wk1/d1/loops-basic-1.py
@@ -1,25 +1,23 @@
-# # Print all integers from 0 to 150.
-# for count in range(0,150):
-#     print(count)
+# Print all integers from 0 to 150.
+for count in range(0,150):
+    print(count)
 
-# #Print all the multiples of 5 from 5 to 1,000
-# for count in range(5,1000,5):
-#     print(count)
+#Print all the multiples of 5 from 5 to 1,000
+for count in range(5,1000,5):
+    print(count)
 
-# # Counting, the Dojo Way
-# for count in range(1,100):
-#     if count % 10  == 0:
-#         print('Coding Dojo')
-#     elif count % 5 == 0:
-#         print('Coding'
+# Counting, the Dojo Way
+for count in range(1,100):
+    if count % 10  == 0:
+        print('Coding Dojo')
+    elif count % 5 == 0:
+        print('Coding')
 
-# #countdown by fours
-# for count in range(2018,4,-4):
-#     print(count)
+#countdown by fours
+for count in range(2018,4,-4):
+    print(count)
 
-
-
-
+#woah suckers huge
 sum = 0
 for count in range(1,500000,2):
     if count % 2 != 0:
@@ -28,12 +26,12 @@ print (sum)
 
 
 
-
-
-
-low_num = 0
-high_num =3
-varmult = 10
-
+#flexible counter
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range(lowNum, highNum + 1):
+    if(i % mult == 0):
+        print(i)
 
 

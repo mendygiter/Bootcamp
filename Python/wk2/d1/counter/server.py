9a1f8c3255032800cfc062735e68be0b52c41cba
Mendy Giter <mendy@leskanharvax.com>
2022-05-26 14:07:55 -0400

    commit

--- Python/wk2/d1/counter/server.py
+++ Python/wk2/d1/counter/server.py
@@ -1,8 +1,21 @@
-from typing import Counter
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'dont share with anyone'
 
 @app.route('/')
 def counter():
+    if 'counts' in session:
+        session['counts'] = session['counts'] + 1
+    else:
+        session['counts'] = 1
     return render_template('index.html')
+
+@app.route('/reset')
+def reset():
+    session['counts'] = 0
+
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug = True)
+
